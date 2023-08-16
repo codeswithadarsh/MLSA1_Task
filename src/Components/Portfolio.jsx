0faba4318cd5b8919bdd9c42a5c054e2fed96523
Adarsh Pandey <meadarshpandey@gmail.com>
2023-08-16 12:15:07 +0530

    add all changes

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/webdesign.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "Web Design and develop";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Advanced Calculator Website",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "I have created a Advanced Calculator with Dark Mode Support. Anyone can use this calculator.",
+    url: "https://codeswithadarsh.github.io/advanced-calculator/",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Advanced Portfolio web",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "I have also created an Advanced Portfolio for myself. I have added my address, experiences, School, etc.",
+    url: "https://codeswithadarsh.github.io/CodSoft-Level-3-Portfolio/",
   },
   {
-    title: "My Resume Site",
+    title: "Flight Landing Page",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "This Landing Page belongs oto the Flight Booking Management. It is created with HTML, CSS.",
+    url: "https://codeswithadarsh.github.io/Landing-Page-CodSoft/",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "A Simple Blog Web",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "In the Accio Contest, I have created a Simple Blog using the HTML and CSS.",
+    url: "https://codeswithadarsh.github.io/AccioContest-Aug1/",
   },
 ];
 
